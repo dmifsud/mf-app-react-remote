75c16c306f97ae1141a4f3ce3da636d662ab4e2f
David Mifsud <david.mifsud@grokacademy.org>
2024-05-31 10:15:36 +0930

    feat: add wip view all

--- src/components/courses/CourseActivities.tsx
+++ src/components/courses/CourseActivities.tsx
@@ -33,12 +33,13 @@ const CourseActivities = ({
               <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
                 {title}
               </h5>
-              {/* <a
+              <a
                 href="#"
+                onClick={(e) => { e.preventDefault(); alert('Coming soon');}}
                 className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
               >
                 View all
-              </a> */}
+              </a>
             </div>
             <div className="flow-root">
               <ul
